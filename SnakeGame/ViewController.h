a9ccf78e1f2484ab5dc0535abb294111a71c6ee8
vin-zhou <wenshanzh@qq.com>
2014-12-25 09:12:35 +0800

    Initial Commit

---
+++ SnakeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnakeGame
+//
+//  Created by Zhou, Wenshan on 12/25/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SnakeGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SnakeGame
+//
+//  Created by Zhou, Wenshan on 12/25/14.
+//  Copyright (c) 2014 MicroStrategy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
